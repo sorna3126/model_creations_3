6c53e7f174e2a70a1eaf215d2f30124b62e9869e
sorna3126 <sornak3105@gmail.com>
2023-12-14 15:31:42 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from app.models import *
 # Create your views here.
+
+def display_country(request):
+
+    QLCO =Country.objects.all()
+
+    d={'Code':QLCO}
+
+    return render(request,'display_country.html',d)
+
+
+def display_capital(request):
+
+    QLCAO=Capital.objects.all()
+
+    d={'Capitals':QLCAO}
+    
+    return render(request,'display_capital.html',d)
--- project19/urls.py
+++ project19/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from app.views import *
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('display_country/',display_country,name='display_country'),
+    path('display_capital/',display_capital,name='display_capital'),
 ]
